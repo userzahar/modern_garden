d4d6abef3f912316363bffe384cf15c1cbb3670f
userzahar <110730909+userzahar@users.noreply.github.com>
2023-08-18 16:38:23 +0300

    add jakor

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelector('.btn-close').addEventListener('click', function (event) {
+    event.preventDefault();
+});
+
+console.log(document.querySelector('.btn-close'))
+
+document.querySelector('.offcanvas-backdrop').addEventListener('click', function (event) {
+    event.preventDefault();
+});
