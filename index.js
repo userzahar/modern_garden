65315b8b53706c56a4d78534cacb3242f16e43f0
userzahar <110730909+userzahar@users.noreply.github.com>
2023-08-20 14:11:52 +0300

    add gellery zwin rozwin

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
-document.querySelector('.btn-close').addEventListener('click', function (event) {
-    event.preventDefault();
-});
 
-console.log(document.querySelector('.btn-close'))
+const openOffcanvasButton = document.querySelector(".offcanvasCloseJs");
 
-document.querySelector('.offcanvas-backdrop').addEventListener('click', function (event) {
-    event.preventDefault();
-});
+openOffcanvasButton.addEventListener('click', chekBackdrop);
+
+function chekBackdrop  () {
+    const backdropOffcanvas = document.querySelector('.offcanvas-backdrop');
+    let scrollPosition = 0;
+    
+    backdropOffcanvas.addEventListener('click', function (event) {
+        event.preventDefault();
+        scrollPosition = window.scrollY;
+        setTimeout( function scroll() {
+            window.scrollTo(0, scrollPosition)},320)
+    } )
+    document.querySelector('.btn-close').addEventListener('click', function (event) {
+        event.preventDefault();
+        scrollPosition = window.scrollY;
+        setTimeout( function scroll() {
+            window.scrollTo(0, scrollPosition)},320)
+    });
+}
+
+const galleryButtonExpand = document.querySelector(".button-expand");
+const galleryButtonCollapse = document.querySelector(".button-collapse");
+const gallery = document.querySelector('.gallery-container');
+const galleryFog = document.querySelector('.gallery-fog');
+
+function openGallery (e){
+    e.preventDefault();
+    gallery.classList.add("isOpen");
+    galleryButtonExpand.classList.add('hide');
+    galleryButtonCollapse.classList.remove('hide');
+    galleryFog.classList.add('isOpen');
+}
+
+function closeGallery (e){
+    e.preventDefault();
+    gallery.classList.remove("isOpen");
+    galleryButtonExpand.classList.remove('hide');
+    galleryButtonCollapse.classList.add('hide');
+    galleryFog.classList.remove('isOpen');
+}
+
+galleryButtonExpand.addEventListener('click', openGallery);
+galleryButtonCollapse.addEventListener('click', closeGallery);
+
+
+
